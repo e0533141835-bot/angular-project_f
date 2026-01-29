85ab227438a1f769c7c5607cf11643cff25cbce0
efrat <E0533141835@gmail.com>
2026-01-30 00:50:24 +0200

    E

--- client/task-manager/src/environments/environment.development.ts
+++ client/task-manager/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://tasks-teacher-server.onrender.com/api'
+    apiUrl: 'http://localhost:3000/api' 
+
 };
--- client/task-manager/src/environments/environment.ts
+++ client/task-manager/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api' 
+  apiUrl: 'https://tasks-teacher-server.onrender.com/api'
+
 };
