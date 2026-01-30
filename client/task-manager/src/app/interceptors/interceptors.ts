fc73b38d5e8baeeb0b9e48d22866be29bda21a86
efrat <E0533141835@gmail.com>
2026-01-30 14:37:25 +0200

    interceptors

--- client/task-manager/src/app/interceptors/interceptors.ts
+++ client/task-manager/src/app/interceptors/interceptors.ts
@@ -1,56 +1,19 @@
-// import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
-// import { inject } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { catchError, throwError } from 'rxjs';
-
-// export const authInterceptor: HttpInterceptorFn = (req, next) => {
-//   const token = sessionStorage.getItem('token');
-//   if (token) {
-//     req = req.clone({
-//       setHeaders: { Authorization: `Bearer ${token}` }
-//     });
-//   }
-//   return next(req);
-// };
-
-// export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-//   const router = inject(Router);
-
-//   return next(req).pipe(
-//     catchError((error: HttpErrorResponse) => {
-
-//       if (error.status === 401) {
-//         alert('שגיאת הרשאה (401): עליך להתחבר מחדש למערכת.');
-//         sessionStorage.removeItem('token');
-//         router.navigate(['/register']);
-//       }
-
-//       else if (error.status === 403) {
-//         alert('⛔ אין לך הרשאות לבצע פעולה זו!');
-//       }
-
-//       else if (error.status === 404) {
-//         alert('🔍 הנתון שחיפשת לא נמצא במערכת (404).');
-//       }
-
-//       else if (error.status === 500) {
-//         alert('🔥 אופס... יש תקלה בשרת. נסה שוב מאוחר יותר.');
-//       }
-
-//       else if (error.status === 0) {
-//         alert('⚠️ אין תקשורת לשרת. בדוק את החיבור שלך.');
-//       }
-
-//       return throwError(() => error);
-//     })
-//   );
-// };
-
 import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { ToastService } from '../services/toast.service'; // <--- ייבוא
+
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = sessionStorage.getItem('token');
+  if (token) {
+    req = req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` }
+    });
+  }
+  return next(req);
+};
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
@@ -60,13 +23,13 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
     catchError((error: HttpErrorResponse) => {
 
       if (error.status === 401) {
-        toast.error('החיבור פג תוקף, אנא התחבר מחדש 🔒'); // <--- שינוי
+         toast.error('החיבור פג תוקף, אנא התחבר מחדש 🔒');
         sessionStorage.removeItem('token');
         router.navigate(['/register']);
       }
 
       else if (error.status === 403) {
-        toast.error('אין לך הרשאות לבצע פעולה זו! ✋');
+         toast.error('אין לך הרשאות לבצע פעולה זו! ✋');
       }
 
       else if (error.status === 404) {
@@ -82,6 +45,9 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
       }
 
       return throwError(() => error);
+
     })
   );
 };
+
+
